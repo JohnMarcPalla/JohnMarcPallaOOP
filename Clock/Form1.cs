dbdf84b5154eb112aa51db961ec4edb77f317a47
John Marc Palla <57927508+JohnMarcPalla@users.noreply.github.com>
2020-01-18 20:13:27 +0800

    Add files via upload
    My own talking clock project

---
+++ Clock/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech;
+using System.Speech.Synthesis;
+
+namespace Clock
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SpeechSynthesizer reader = new SpeechSynthesizer();
+        public void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Text = DateTime.Now.ToString("hh:mm  tt");
+            label2.Text = DateTime.Now.ToString("ss");
+            label3.Text = DateTime.Now.ToString("MMMM dd, yyyy");
+            label4.Text = DateTime.Now.ToString("dddd");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (label1.Text != "") 
+            {
+                reader.Dispose();
+                reader = new SpeechSynthesizer();
+                reader.SpeakAsync(label1.Text);
+            }
+
+        }
+
+
+    }
+}
